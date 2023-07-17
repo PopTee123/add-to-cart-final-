a47ad68b057fa44b7c3181ee6512e81e4aa5b9ee
PopTee123 <90551038+PopTee123@users.noreply.github.com>
2023-07-17 10:46:57 +0100

    Add files via upload

---
+++ Mobile-app/index.js
@@ -0,0 +1,120 @@
+
+
+// Challenge: Import 'initializeApp' from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-9c24f-default-rtdb.firebaseio.com/"
+}
+
+ 
+
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+
+
+
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
+})
+
+/*
+Challenge:
+Call the onValue function with
+shoppingListInDB as the first argument and
+function(snapshot) {} as the second argument
+*/
+
+
+
+onValue(shoppingListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearShoppingListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToShoppingListEl(currentItem)
+        }    
+    } else {
+        shoppingListEl.innerHTML = "No items here... yet"
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+
+function clearInputFieldEl() {
+    inputFieldEl.value = " "
+}
+
+
+
+
+
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    // shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+    
+    let newEl = document.createElement("li")
+    // Challenge: Make it so the item name shows instead of 'Something'
+    
+    newEl.textContent = itemValue
+    
+    
+            // Challenge: Attach an event listener to newEl and 
+            // make it so you console log the id of the item when it's pressed
+    newEl.addEventListener("click", function() {
+        // Challenge: Make a let variable called 'exactLocationOfItemInDB' 
+        // and set it equal to ref(database, something) where you substitute 
+        // something with the code that will give you the exact location of 
+        // the item in question.       
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        // Challenge: Use the remove function to remove the item from the database
+        
+        remove(exactLocationOfItemInDB)
+    })
+    
+    shoppingListEl.append(newEl)
+}
+
+
+
+
+
+
+// =================================
+
+// onValue(booksInDB, function(snapshot) {
+//     let booksArray = Object.values(snapshot.val())
+//           clearBooksListEl()
+    
+//     // Challenge: Write a for loop where you console log each book.
+//     for(let i = 0; i < booksArray.length; i++){
+        
+//         let currentBook = booksArray[i]
+//     appendBookToBooksListEl(currentBook)
+   
+//     }
+// })
